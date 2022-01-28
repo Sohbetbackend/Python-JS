8ab9d4d5dbdd397c533bc1190cd32fe14db7c0b8
ilmuradovv <perdaev55@gmail.com>
2022-01-28 14:28:07 +0500

    add

--- app/static/messages.js
+++ app/static/messages.js
@@ -1,11 +1,18 @@
 const messages = document.getElementById('messages')
 const error = document.getElementById('error')
-const height = messages.clientHeight
+// const height = messages.clientHeight
 
 
 window.addEventListener('DOMContentLoaded', () => {
-    if(height === 0) {
-        error.style.display = 'flex';
+    if (messages == null) {
+        // messages.clientHeight = 1;
+        error.style.display = 'flex'
     }
+
+    // if(height) {
+    //     error.style.display = 'none';
+    // } else {
+    //     error.style.display = 'flex'
+    // }
 })
 
